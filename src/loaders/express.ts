38cd5e2e4a6efe664f6921259826318799ed9776
Delache <erique.delacharlerie@gmail.com>
2020-03-21 23:25:20 +0100

    feat: swaggerConfig

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,7 +3,9 @@
   import cors from 'cors';
   import express, { Application } from 'express';
   import { env } from '../core/environnement';
+  import swaggerUi from 'swagger-ui-express';
   import path from 'path';
+  import swaggerDocument from '../../swagger.json';
   export default async ( app: Application) => {
 
     app.get('/status', (req, res) => { res.status(200).end(); });
@@ -11,6 +13,7 @@
     app.use(cors());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     const p = path.join(__dirname, '../../' + env.uploadRecipeFolder);
     app.use('/recipesuploads', express.static(p));
